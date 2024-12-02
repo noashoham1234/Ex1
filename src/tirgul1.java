92805f3d580f354d2ea6e7b343cec0d928d5415a
noash <noashoham6@gmail.com>
2024-12-02 14:49:36 +0200

    123

---
+++ src/tirgul1.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class tirgul1 {
+    public  static int maxRow(int[][] a){
+        int x = 0;
+
+        return x;
+    }
+
+    @Test
+    void testMaxRow(){
+
+    }
+}
