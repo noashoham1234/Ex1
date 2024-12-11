9407d7236edca284022e6dd21a3fec26c0839faf
noash <noashoham6@gmail.com>
2024-12-11 18:43:26 +0200

    fixed main

--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -18,36 +18,36 @@ public class Ex1Main {
             num1 = sc.next();
             boolean isNum1 = Ex1.isNumber(num1);
             int valueNum1 = Ex1.number2Int(num1);
-            if (isNum1) {
+            if (!isNum1) {
+                System.out.println("ERR: num1 is in the wrong format! (" + num1 + ")");
+                continue;
+            } else {
                 if (!num1.equals("quit")) {
                     System.out.println("num1 = " + num1 + " is number:" + isNum1 + "," + valueNum1);
                 } else {
                     break;
                 }
-            } else {
-                System.out.println("ERR: num1 is in the wrong format! (" + num1 + ")");
-                continue;
             }
 
             System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
             num2 = sc.next();
             boolean isNum2 = Ex1.isNumber(num2);
             int valueNum2 = Ex1.number2Int(num2);
-            if (isNum2) {
+            if (!isNum2) {
+                System.out.println("ERR: num2 is in the wrong format! (" + num2 + ")");
+                continue;
+            } else {
                 if (!num2.equals("quit")) {
                     System.out.println("num2 = " + num2 + " is number:" + isNum2 + "," + valueNum2);
                 } else {
                     break;
                 }
-            } else {
-                System.out.println("ERR: num2 is in the wrong format! (" + num2 + ")");
-                continue;
             }
             System.out.println("Enter a base for output: (a number [2,16])");
             int base = Integer.parseInt(sc.next());
-            String valueSumPluse = Ex1.int2Number(Ex1.number2Int(num1) + Ex1.number2Int(num2), base);
-            String valueSumMult = Ex1.int2Number(Ex1.number2Int(num1) * Ex1.number2Int(num2), base);
-            String[] arr = {num1, num2, valueSumPluse, valueSumMult};
+            String valueSumPlus = Ex1.int2Number(Ex1.number2Int(num1) + Ex1.number2Int(num2), base);
+            String valueSumMulti = Ex1.int2Number(Ex1.number2Int(num1) * Ex1.number2Int(num2), base);
+            String[] arr = {num1, num2, valueSumPlus, valueSumMulti};
             int maxIndex = Ex1.maxIndex(arr);
             String maxNum = arr[maxIndex];
             if (!Ex1.checkIfValidBase(base)) {
@@ -55,8 +55,8 @@ public class Ex1Main {
                 continue;
 
             } else {
-                System.out.println(num1 + " + " + num2 + " = " + valueSumPluse);
-                System.out.println(num1 + " * " + num2 + " = " + valueSumMult);
+                System.out.println(num1 + " + " + num2 + " = " + valueSumPlus);
+                System.out.println(num1 + " * " + num2 + " = " + valueSumMulti);
                 System.out.println("Max number over " + Arrays.toString(arr) + "is: " + maxNum);
             }
         }
