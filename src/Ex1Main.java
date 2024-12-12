5e364d32b20a0cae6c9260d2b83df5e0890f5d0c
noash <noashoham6@gmail.com>
2024-12-12 13:37:30 +0200

    fixed small details in main

--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -43,19 +43,22 @@ public class Ex1Main {
                     break;
                 }
             }
-            System.out.println("Enter a base for output: (a number [2,16])");
-            int base = Integer.parseInt(sc.next());
+            System.out.println("Enter a base for output (or \"quit\" to end the program): (a number [2,16])");
+            String firstBase = sc.next();
+            int base = Ex1.convertBaseStringToNum(firstBase);
             String valueSumPlus = Ex1.int2Number(Ex1.number2Int(num1) + Ex1.number2Int(num2), base);
             String valueSumMulti = Ex1.int2Number(Ex1.number2Int(num1) * Ex1.number2Int(num2), base);
             String[] arr = {num1, num2, valueSumPlus, valueSumMulti};
             int maxIndex = Ex1.maxIndex(arr);
             String maxNum = arr[maxIndex];
-            if (!Ex1.checkIfValidBase(base)) {
-                System.out.println("ERR: wrong base, should be [2,16], got (" + base + ")");
+            if (firstBase.equals("quit")) {
+                break;
+            } else if (!Ex1.checkIfValidBase(base)) {
+                System.out.println("ERR: wrong base, should be [2,16], got (" + firstBase + ")");
             } else {
                 System.out.println(num1 + " + " + num2 + " = " + valueSumPlus);
                 System.out.println(num1 + " * " + num2 + " = " + valueSumMulti);
-                System.out.println("Max number over " + Arrays.toString(arr) + "is: " + maxNum);
+                System.out.println("Max number over " + Arrays.toString(arr) + " is: " + maxNum);
             }
         }
         System.out.println("quiting now...");
